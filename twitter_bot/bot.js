483e3af069c9eaaa24cfb3c0336ff89745e0eda7
adt <aarondtaveras@gmail.com>
2018-06-12 11:52:15 -0400

    First post method

--- twitter_bot/bot.js
+++ twitter_bot/bot.js
@@ -1,7 +1,28 @@
 var Twit = require('twit');
 console.log("Initializing bot...");
 
-var config = require('./config');
+var config = require('./config.js');
 
-var T = new Twit(config.config);
+var T = new Twit(config);
+
+tweeter();
+
+setInterval(tweeter, 60*1000);
+
+function helloWorld(){
+
+    var tweet = 'hello world';
+
+    T.post('statuses/update', {status: tweet}, tweeted);
+
+    function completedTweet(err,data,response){
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log('Success: ' + data.text);
+        }
+    };
+
+}
 
