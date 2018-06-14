ff34df08462e933da57a0d36ec60727d643f18c7
adt <aarondtaveras@gmail.com>
2018-06-14 14:45:44 -0400

    sql lite demo

---
+++ sqlite_demo/careers_data.sql
@@ -0,0 +1,9 @@
+/*To run this file, open up your database and run:sqlite>  */
+-- $ sqlite3 careers_data.db
+-- $ .read careers_data.sql
+/* Set settings */
+-- $ .headers on
+-- $ .mode column
+-- $ .echo on
+/* See all the tables */
+-- $ .tables
